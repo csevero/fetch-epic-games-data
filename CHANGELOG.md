65b3f4fa8d026d0f43a3ac47c4721137e1304e65
kissinger156 <severo.e.carlos@gmail.com>
2023-05-20 12:17:22 -0300

    update: change to show information during the script running and file handling

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ## 2022-05-20
 
+### 1.1.0
+
+- 🚀 Add ora lib to show the progress of the script and information after finalizing the script running
+- 🔄 Delete the games-info.csv file if it exists and create a new empty one at the start of the script running
 ### 1.0.0
 
 - 🚀 Initial script configuration
--- src/index.js
+++ src/index.js
@@ -1,39 +1,64 @@
-import axios from 'axios'
-import { config } from 'dotenv'
-import { appendFileSync,existsSync, writeFileSync } from 'fs'
+import axios from 'axios';
+import { config } from 'dotenv';
+import { appendFileSync, existsSync, writeFileSync, rmSync } from 'fs';
+import ora from 'ora';
 
 config();
 
 async function* getEpicGamesData() {
-  let url = 'https://www.epicgames.com/account/v2/payment/ajaxGetOrderHistory?sortDir=DESC&sortBy=DATE&locale=pt-BR'
+  let url =
+    'https://www.epicgames.com/account/v2/payment/ajaxGetOrderHistory?sortDir=DESC&sortBy=DATE&locale=pt-BR';
 
   while (url) {
-    const { data } = await axios.get(url, { headers: { cookie: process.env.COOKIE } })
+    const { data } = await axios.get(url, {
+      headers: { cookie: process.env.COOKIE },
+    });
 
-    yield data.orders.map(order => ({ name: order.items[0].description, price: order.items[0].amount, date: new Date(order.createdAtMillis).toLocaleDateString('pt-br') }))
+    yield data.orders.map(order => ({
+      name: order.items[0].description,
+      price: order.items[0].amount,
+      date: new Date(order.createdAtMillis).toLocaleDateString('pt-br'),
+    }));
 
     if (!data.nextPageToken) break;
 
     if (url.includes('nextPageToken')) {
-      url = url.replace(/(nextPageToken=)(.*)/, `$1${data.nextPageToken}`)
+      url = url.replace(/(nextPageToken=)(.*)/, `$1${data.nextPageToken}`);
     } else {
-      url = url.concat(`&nextPageToken=${data.nextPageToken}`)
+      url = url.concat(`&nextPageToken=${data.nextPageToken}`);
     }
   }
 }
 
 function writeDataToCsv(contents) {
-  if(!existsSync('./src/assets/games-info.csv')) {
-    writeFileSync('./src/assets/games-info.csv', 'Name;Price;Date\n')
+  if (existsSync('./src/assets/games-info.csv')) {
+    rmSync('./src/assets/games-info.csv');
   }
 
-  const contentFormatted = contents.map(content => Object.values(content).join(';')).join('\n')
+  writeFileSync('./src/assets/games-info.csv', 'Name;Price;Date\n');
 
-  appendFileSync(`./src/assets/games-info.csv`, contentFormatted.concat('\n'))
+  const contentFormatted = contents
+    .map(content => Object.values(content).join(';'))
+    .join('\n');
+
+  appendFileSync(`./src/assets/games-info.csv`, contentFormatted.concat('\n'));
 }
 
 (async () => {
+  const spinner = ora();
+  let gamesCount = 0;
+  let gamesTotalPrice = 0;
+
+  spinner.start('🤖 - Fetching your games');
   for await (let data of getEpicGamesData()) {
-    writeDataToCsv(data)
+    writeDataToCsv(data);
+
+    const pageTotalPrice = data.reduce((prev,cur) => Number(cur.price.replace('R$','').replace(',','.')) + prev, 0)
+
+    gamesTotalPrice += pageTotalPrice;
+    gamesCount += data.length;
+    spinner.text = `🔎 - ${gamesCount} fetched`;
   }
-})()
+  spinner.succeed(`✅ - ${gamesCount} fetched`);
+  spinner.succeed(`✅ - Total price of your account R$ ${gamesTotalPrice.toFixed(2)}`);
+})();
